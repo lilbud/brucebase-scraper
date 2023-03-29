d5e0fa5bbb167da9cc01a5eb903e0db9de538766
lilbud <bvw20002010@gmail.com>
2023-03-29 00:56:16 -0400

    update to fix file handling

--- scraper.py
+++ scraper.py
@@ -1,10 +1,11 @@
 import requests, re, os, sys
 from bs4 import BeautifulSoup as bs4, SoupStrainer as strainer
 
-global saveDir, URL, show, incHeaders, incSoundcheck, months
+global saveDir, URL, show, incHeaders, incSoundcheck, months, index
 saveDir = os.environ["USERPROFILE"] + "\Documents\BB_Setlists\\"
 URL = "http://brucebase.wikidot.com"
 show = []
+incHeaders = incSoundcheck = ""
 months = ['_None', 'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
 
 print("Brucebase Setlist Scraper: Gets Setlists From brucebase.wikidot.com")
@@ -16,7 +17,12 @@ except:
 	showDate = input("\nEnter Date: ")
 
 incHeaders = input("Include Headers (Soundcheck, Different Artists, etc.)? [Y/N]: ")
+while incHeaders == "":
+	incHeaders = input("Include Headers (Soundcheck, Different Artists, etc.)? [Y/N]: ")
+
 incSoundcheck = input("Include Soundcheck? [Y/N]: ")
+while incSoundcheck == "":
+	incSoundcheck = input("Include Soundcheck? [Y/N]: ")
 
 def dateCheck(showDate):
 	if (1965 <= int(showDate[0:4]) <= 2023):
@@ -30,7 +36,7 @@ def dateCheck(showDate):
 					return True
 
 def titleCase(songName):
-	return songName.title().replace("'S", "'s").replace("Th ", "th ").replace("Nd", "nd").replace("Ll", "ll").replace("'T", "'t").replace("'M", "'m")
+	return songName.title().replace("'S", "'s").replace("Th ", "th ").replace("Nd", "nd").replace("Ll", "ll").replace("'T", "'t").replace("'M", "'m").replace("'D", "'d").replace("'Ve", "'ve")
 
 if not dateCheck(showDate):
 	print("ERROR: Invalid Date")
@@ -39,24 +45,26 @@ if not dateCheck(showDate):
 yearURL = requests.get(URL + "/" + showDate[0:4])
 soupYear = bs4(yearURL.content, "lxml")
 
-def fileOutput(pageTitle):
+def fileOutput(show, pageTitle, index):
 	dir = saveDir + pageTitle[0:4] + "\\" + pageTitle[5:7] + "_" + months[int(pageTitle[5:7])] + "\\"
-	index = 1
 	fName = dir + pageTitle[0:11].strip()
 	
 	os.makedirs(dir, exist_ok=True)
 
-	if len(show) > 0:	
-		while (os.path.exists(fName + "_" + str(index) + ".txt")):
-			index += 1
-
-		f = open(fName + "_" + str(index) + ".txt", "w")
+	if len(show) > 0:
+		if index == 0:
+			f = open(fName + ".txt", "w")
+		else:
+			f = open(fName + "_" + str(index) + ".txt", "w")
 
 		if incHeaders.upper() == "Y":
 			f.write(pageTitle + "\n")
 
 		for song in show:
-			f = open(fName + "_" + str(index) + ".txt", "a")
+			if index == 0:
+				f = open(fName + ".txt", "a")
+			else:
+				f = open(fName + "_" + str(index) + ".txt", "a")
 
 			if ":" in song:
 				f.write("\n" + song + "\n")
@@ -64,10 +72,11 @@ def fileOutput(pageTitle):
 				f.write(song + "\n")
 			f.close()
 
-		print(f.name + " successfully created")
+		print("\n" + f.name + " successfully created")
+		print("-" * 20 + "\n")
 
 def showPrint(show, pageTitle):
-	print("\n" + pageTitle[0:10] + " - " + pageTitle[11:] + "\n")
+	print(pageTitle[0:10] + " - " + pageTitle[11:] + "\n")
 
 	for i, item in enumerate(show):
 		if ":" in item:
@@ -75,9 +84,7 @@ def showPrint(show, pageTitle):
 		else:
 			print(item)
 
-	print("\n" + "-" * 20 + "\n")
-
-def processing(item):
+def processing(item, index):
 	showPage = requests.get(URL + item.get("href"))
 	getSetlist = bs4(showPage.content, "lxml")
 	setlist = getSetlist.find(id="wiki-tab-0-1")
@@ -94,8 +101,11 @@ def processing(item):
 			else:
 				show.append(titleCase(item.text.strip()))
 
+	if len(show) == 0:
+		show.append("Setlist Not Found")
+	
 	showPrint(show, pageTitle)
-	fileOutput(pageTitle)
+	fileOutput(show, pageTitle, index)
 
 match len(showDate):
 	case 10:
@@ -106,7 +116,9 @@ match len(showDate):
 	case 7:
 		showFind = soupYear.find_all("a", string=re.compile(showDate[0:7] + ".*"))
 
+index = 0
 for item in showFind:
 	if "/gig:" in item.get("href"):
-		processing(item)
+		processing(item, index)
 		show = []
+		index += 1
